094ca2b2d714b55d4045bd9e83c3281267c802a8
Counterweight15 <62943799+Counterweight15@users.noreply.github.com>
2020-03-31 15:41:20 +0200

    Update cws-notice.js

--- cws-notice.js
+++ cws-notice.js
@@ -1,5 +1,5 @@
 var html = '<div class="cws-notice" style="background-color: #004020; padding: 15px; display: inline-block; width: 100%;">';
-html += '<p style="text-align: center; color: white; margin: 0 0 5px 0; font-size: 20px;"><a style="color: white; text-decoration: none;" href="https://sacoronavirus.co.za/" target="_blank"><img style="width: 100%; max-width: 450px;" src="https://www.cws15.com/covid19notice/sacoronavirus2.png" alt"Visit for more information" /></a></p>';
+html += '<p style="text-align: center; color: white; margin: 0 0 5px 0; font-size: 20px;"><a style="color: white; text-decoration: none;" href="https://sacoronavirus.co.za/" target="_blank"><img style="width: 100%; max-width: 450px;" src="https://counterweight15.github.io/covid-19/sacoronavirus2.png" alt"Visit for more information" /></a></p>';
 html += '<p style="text-align: center; color: white; margin: 0; font-size: 11px; line-height: 16px;">Enforced by the Department of Health South Africa. Get this notice for your site at <a style="color: white; text-decoration: underline;" href="https://web.counterweight.co.za/covid-19/" target="_blank">CWS</a></p>'
 html += '</div>';
 
